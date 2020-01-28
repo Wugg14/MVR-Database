74b5d85c585483041b62aa3d28d9a9c0cd5ab22e
Mark <mark.steininger@gmail.com>
2020-01-28 17:23:30 -0500

    JS class now can populate invoice fields based on the reports table

--- app/static/js/invoice.js
+++ app/static/js/invoice.js
@@ -2,19 +2,62 @@
 class InvoiceForm {
 
     constructor() {
-        this.buttons = document.getElementsByClassName('add-to-invoice');
+        this.slot1 = document.getElementById('slot-1');
+        this.slot2 = document.getElementById('slot-2');
+        this.slot3 = document.getElementById('slot-3');
+
     }
 
      // Events
     events() {
-
     }
 
     //Methods
-    test(item){
-        console.log(item)
-       let z = item.dataset.reportid;
+    dispatcher(item){
+        let z = item.dataset.reportid;
+        console.log(z);
+        let slot = this.identifySlot();
+        this.fillSlot(slot, item)
     }
+
+    identifySlot(){
+        if (!this.slot1.classList.contains('filled')) {
+            return this.slot1
+        }
+
+        else if(!this.slot2.classList.contains('filled')) {
+            return this.slot2
+        }
+
+        else if(!this.slot3.classList.contains('filled')) {
+            return this.slot3
+        }
+    }
+
+    fillSlot(slot, item){
+        let idSlot = slot.querySelector('.reportID');
+        let idField = idSlot.querySelector('input');
+        idField.value = item.dataset.reportid;
+
+        let patientSlot = slot.querySelector('.reportPatient');
+        let patientField = patientSlot.querySelector('input');
+        patientField.value = item.dataset.reportpatient;
+
+        let doctorSlot = slot.querySelector('.reportDoctor');
+        let doctorField = doctorSlot.querySelector('input');
+        doctorField.value = item.dataset.reportdoctor;
+
+        let serviceSlot = slot.querySelector('.reportService');
+        let serviceField = serviceSlot.querySelector('input');
+        serviceField.value = item.dataset.reportservice;
+
+        let priceSlot = slot.querySelector('.reportPrice');
+        let priceField = priceSlot.querySelector('input');
+        priceField.value = item.dataset.reportprice;
+
+        slot.classList.add("filled");
+    }
+
 
 }
 
